038e7178ccab7a11ce815fb1bf0e31e2779ac4be
Esref Ozturk <esrefozturk93@gmail.com>
2017-12-15 13:57:06 +0300

    error handling

--- checker.py
+++ checker.py
@@ -4,14 +4,22 @@ EXTENSIONS = ['com', 'net', 'io', 'ai']
 
 
 def whois(domain, extension):
-    out, err = Popen('whois {d}.{e} | grep -i "No match for domain"'.format(d=domain, e=extension), shell=True,
-                     stdout=PIPE, stderr=PIPE).communicate()
+    ctr = 5
+    while ctr >= 0:
+        ctr -= 1
+        try:
+            out, err = Popen('whois {d}.{e} | grep -i "No match for domain"'.format(d=domain, e=extension), shell=True,
+                             stdout=PIPE, stderr=PIPE).communicate()
+        except Exception as e:
+            print 'Exception:', e
 
-    if err:
-        print err
+        if err:
+            print 'Error', err
 
-    if out:
-        return True
+        if out:
+            return True
+        else:
+            return False
 
 
 def write(domain, extension):
@@ -19,12 +27,18 @@ def write(domain, extension):
     f.write('{d}.{e}\n'.format(d=domain, e=extension))
     f.close()
 
+    if 3 <= domain <= 7:
+        f = open('availables-3-7', 'a+')
+        f.write('{d}.{e}\n'.format(d=domain, e=extension))
+        f.close()
+
 
 def check():
     domains = []
     with open('wordlist') as f:
         domains = f.read().strip().split('\n')
     for domain in domains:
+        domain = domain.lower()
         print 'Trying ', domain
         for extension in EXTENSIONS:
             if whois(domain, extension):
